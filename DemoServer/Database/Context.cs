8ee4e026bccdc3de1a8fe85e134a3bf45eb8bd6a
Björn Strömberg <bjorn.stromberg@codic.se>
2021-06-11 15:02:51 +0200

    Add project files.

---
+++ DemoServer/Database/Context.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DemoServer.Database
+{
+    public class Context : DbContext
+    {
+        public Context([NotNull] DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TestModel> TestTable { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TestModel>().HasData(
+                new TestModel { 
+                    Message = "<h1>Testdata i en databas!</h1>" 
+                });
+        }
+    }
+
+    public class TestModel
+    {
+        [Key]
+        public string Message { get; set; }
+    }
+}
---
+++ DemoServer/Startup.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DemoServer
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<Database.Context>(options =>
+                options.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    var dbContext = context.RequestServices.GetRequiredService<Database.Context>();
+                    await dbContext.Database.EnsureCreatedAsync(); // TODO IDbSetup service
+                    var content = await dbContext.TestTable.FirstOrDefaultAsync();
+                    await context.Response.WriteAsync(content.Message);
+                    context.Response.ContentType = "text/html";
+                });
+            });
+        }
+    }
+}
